adbb69b7d4c2051ec5f9b20b8d95b0581a5e319c
luke <114527169+lukeot22@users.noreply.github.com>
2024-02-13 02:17:21 +0400

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="youtube" />
+
+</template>
